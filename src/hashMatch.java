6db0f602fc5972bf2ff579ebfada29abf9d1c3fe
Mackenzie Kudrenecky <mkudrene@student.ubc.ca>
2023-03-06 20:04:25 -0800

    feat: updated .java with comments/remove excess

--- src/hashMatch.java
+++ src/hashMatch.java
@@ -11,15 +11,7 @@ public class hashMatch {
 
     Hashtable<Integer, String> abbrTable = new Hashtable<>();
 
-    public void buildHashTable(String[] abbrList) {
-        for (String word : abbrList) {
-            int key = word.hashCode();
-            // we can use hashCode or not, can also just use the string as the key (as is
-            // done in the sample below)
-            abbrTable.put(key, word);
-        }
-    }
-
+    // method to compare the data and the hashtable - swaps abbreviations with corresponding phrases
     public StringBuilder swap(String inputString, Hashtable<String, String> table) {
         StringBuilder newStr = new StringBuilder();
         String[] wordArr = inputString.split(" |,|\\.|!|\\?|\\(|\\)|\\{|\\}|\\[|\\]");
@@ -34,6 +26,7 @@ public class hashMatch {
         return newStr;
     }
 
+    // method to read in data from txt file - provides workable data
     public static String inputTweetFromTxt(String txtFilePath) {
         String longTweets = "";
         try (Scanner tweets = new Scanner(new FileInputStream(txtFilePath));) {
@@ -48,6 +41,7 @@ public class hashMatch {
         }return longTweets;
     }
 
+    // method to build hash table using data from the abbreviations URL
     public Hashtable<String, String> buildTable(String theUrl) {
 		Hashtable<String, String> abbrTable = new Hashtable<>();
         int count = 0;
@@ -58,14 +52,14 @@ public class hashMatch {
 
 			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
 			String line;
-			
+
 			while ((line = bufferedReader.readLine()) != null && count<50) {
 				String[] abbrArr = line.split("-");
                 if(abbrArr[0]!=null && abbrArr[1]!=null){
                 abbrTable.put(abbrArr[0], abbrArr[1]);
-                System.out.println(abbrArr[0] + "-" + abbrArr[1]);
+                //System.out.println(abbrArr[0] + "-" + abbrArr[1]);
                 }
-                count++;
+                count++;        //counter implemented to offset code reaching an abbreviation with no phrase which crashes
 			}
 			bufferedReader.close();
 		} catch (Exception e) {
@@ -75,6 +69,7 @@ public class hashMatch {
 		return abbrTable;
 	}
 
+    // method to build hash table from input file
     public Hashtable<String, String> buildTableFromFile(String filePath) {
 		Hashtable<String, String> abbrTable = new Hashtable<>();
 		
@@ -83,11 +78,9 @@ public class hashMatch {
 			String line;
 			
 			while ((line = bufferedReader.readLine()) != null) {
-				String[] abbrArr = line.split("-");
-                if(abbrArr[0]!=null && abbrArr[1]!=null){
+				String[] abbrArr = line.split("-");     
                 abbrTable.put(abbrArr[0], abbrArr[1]);
-                System.out.println(abbrArr[0] + "-" + abbrArr[1]);
-                }
+                //System.out.println(abbrArr[0] + "-" + abbrArr[1]);
 			}
 			bufferedReader.close();
 		} catch (Exception e) {
@@ -99,37 +92,27 @@ public class hashMatch {
 
     public static void main(String[] args) {
 
-        //hashMatch test = new hashMatch();
+        // instantiate class and two hashtables 
         hashMatch test2 = new hashMatch();
-       // Hashtable<String, String> abbreviationMap = new Hashtable<>();
         Hashtable<String, String> table = new Hashtable<>();
         Hashtable<String, String> table2 = new Hashtable<>();
 
         // Call input from Excel function to receive input in list
         String tweetsList = inputTweetFromTxt("./Sample_Txt_Input.txt");
 
-        // add some abbreviations and their corresponding phrases to the hash table
-        // abbreviationMap.put("ASAP", "As Soon As Possible");
-        // abbreviationMap.put("FYI", "For Your Information");
-        // abbreviationMap.put("ETA", "Estimated Time of Arrival");
-        // abbreviationMap.put("BTW", "By The Way");
-        // abbreviationMap.put("LOL", "Laugh out loud");
-        // abbreviationMap.put("IMO", "In my opinion");
-        // abbreviationMap.put("RN", "Right now");
-        // abbreviationMap.put("SUS", "Suspicious");
-        // abbreviationMap.put("FML", "F*** my life");
-        // abbreviationMap.put("GOAT", "Greatest of all time");
-
-        // for (String tweet : tweetsList) {
             System.out.println("\nOriginal String: " + tweetsList);
-            System.out.print("\nNew String: ");
-           // test2.swap(tweetsList, abbreviationMap);
+
             table = test2.buildTable("https://raw.githubusercontent.com/krishnakt031990/Crawl-Wiki-For-Acronyms/master/AcronymsFile.csv");
             test2.swap(tweetsList, table);
 
+            // comparison done with hashtable built from text file.
+            System.out.println("New string using txt file table:");
             table2 = test2.buildTableFromFile("./Sample_txt_Abbr2.txt");
             test2.swap(tweetsList, table2);
-            System.out.println("we got here Mac");
-        // }
+            
+            // this call uses table built from URL - only 'AAC' is swapped, as the count reaches 50 and the build ceases - this is to avoid the null value which crashes
+            // the swap function and hashtable are working, but the null value needs to fixed
+            System.out.println("New string using URL build table:");
+            test2.swap(tweetsList,table);
     }
 }
